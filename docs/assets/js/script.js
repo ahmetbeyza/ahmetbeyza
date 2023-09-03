04e880415fefb1740ef7f023e917de4b15ee6f88
Ahmet Kasım Erbay <ahmetkasim2@gmail.com>
2023-09-04 01:51:45 +0300

    update counter

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,27 +1,3 @@
-// document.addEventListener("DOMContentLoaded", (event) => {
-//   typeWriter();
-  
-// });
-
-
-// var i = 0;
-// var txt = 'EVLENİYORUZ'; /* The text */
-// var speed = 250; /* The speed/duration of the effect in milliseconds */
-
-// function typeWriter() {
-//   if (i < txt.length) {
-//     document.getElementById("typing-text").innerHTML += txt.charAt(i);
-//     i++;
-//     setTimeout(typeWriter, speed);
-//   }
-// }
-
-
-
-
-
-
-
 function makeInvitation() {
 
     const guestName = document.getElementById("guestName").value.toUpperCase();
@@ -49,12 +25,10 @@ function makeInvitation() {
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -76,7 +50,7 @@ function showSlides(n) {
 
   
 
-const text = "EVLENİYORUZ"; // The text you want to type
+const text = "EVLENİYORUZ";
 const typingText = document.getElementById('typing-text');
 const backButton = document.getElementById('prev1');
 
@@ -87,21 +61,48 @@ function typeText() {
         if (index < text.length) {
             typingText.textContent += text.charAt(index);
             index++;
-            setTimeout(typeCharacter, 250); // Adjust typing speed (milliseconds per character)
+            setTimeout(typeCharacter, 250); 
         }
     }
 
     typeCharacter();
 }
 
-// Start the typing effect when the page loads
 typeText();
 
-// Event listener for the back button
 backButton.addEventListener('click', function() {
-    // Clear the existing text
     typingText.textContent = '';
     
-    // Start the typing effect again
     typeText();
 });
+
+
+// Counter Code
+// -------------------------------------------------
+var countDownDate = new Date("Oct 28, 2023 19:00:00").getTime();
+
+var date = new Date();
+
+var myfunc = setInterval(function() {
+
+var now = new Date().getTime();
+var timeleft = countDownDate - now;
+
+var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+document.getElementById("day").innerHTML = days 
+document.getElementById("hour").innerHTML = hours 
+document.getElementById("min").innerHTML = minutes
+document.getElementById("sec").innerHTML = seconds
+
+if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.getElementById("day").innerHTML = "Are"
+    document.getElementById("hour").innerHTML = "You" 
+    document.getElementById("min").innerHTML = "Ready"
+    document.getElementById("sec").innerHTML = "To Wed?"
+  }
+}, 1000);
