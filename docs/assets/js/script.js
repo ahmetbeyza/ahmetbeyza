112ddff17608bf06713af9fd863cef3e2ac0d27a
ahmetbeyza <ahmetkasimhaticebeyza@gmail.com>
2023-09-03 23:36:02 +0300

    bg

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,3 +1,27 @@
+// document.addEventListener("DOMContentLoaded", (event) => {
+//   typeWriter();
+  
+// });
+
+
+// var i = 0;
+// var txt = 'EVLENİYORUZ'; /* The text */
+// var speed = 250; /* The speed/duration of the effect in milliseconds */
+
+// function typeWriter() {
+//   if (i < txt.length) {
+//     document.getElementById("typing-text").innerHTML += txt.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+
+
+
+
+
+
 function makeInvitation() {
 
     const guestName = document.getElementById("guestName").value.toUpperCase();
@@ -25,29 +49,31 @@ function makeInvitation() {
 let slideIndex = 1;
 showSlides(slideIndex);
 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
 function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "initial";
-    dots[slideIndex-1].className += " active";
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
 
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
-
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
   
 
 const text = "EVLENİYORUZ"; // The text you want to type
